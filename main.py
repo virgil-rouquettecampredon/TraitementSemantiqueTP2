f1aec079a248ef69b4cc1a20ddacb958bbe9eee6
Virgil LAPTOP 2 <virgil.rouquette-campredon@etu.umontpellier.fr>
2022-02-28 13:14:03 +0100

    WIP DEV code

--- main.py
+++ main.py
@@ -16,6 +16,51 @@ def existSame(s1, s2):
     intersection = set(s1).intersection(s2)
     return list(intersection)
 
+def getAllTypes(graph):
+    req = """  
+            SELECT DISTINCT ?type
+            WHERE {
+                ?s a ?type.
+            }
+          """
+    result = graph.query(req)
+    print("Nombre types : " + str(len(result)))
+    for row in result:
+        print(row)
+    return result
+
+def getAllGenres(graph):
+    #Retourne tous les genres de tous les F22_Self-Contained_Expression du graph
+    req = """  
+    PREFIX mus: <http://data.doremus.org/ontology#>
+    PREFIX ecrm:  <http://erlangen-crm.org/current/>
+    PREFIX efrbroo: <http://erlangen-crm.org/efrbroo/>
+    PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+
+    SELECT DISTINCT ?genre ?z
+    WHERE {
+        ?x a efrbroo:F22_Self-Contained_Expression ;
+        mus:U12_has_genre ?genre .
+        OPTIONAL {
+            SELECT ?genre ?z
+            WHERE {
+                ?genre a mus:M5_Genre ;
+                ecrm:P1_is_identified_by ?z;
+            }
+        }
+    }
+    """
+
+    #mus:U12_has_genre [ a mus:M5_Genre ;
+    #                      ecrm:P1_is_identified_by  "musique contemporaine"@fr
+    #                  ] ;
+
+    result = graph.query(req)
+
+    print("Nombre genres : " + str(len(result)))
+    for row in result:
+        print(row)
+    return result
 
 
 if __name__ == '__main__':
@@ -41,6 +86,7 @@ if __name__ == '__main__':
     }
     """
 
+    #Obtenir Toutes les propriétés d'un type efrbroo:F22_Self-Contained_Expression
     q2 = """
     PREFIX mus: <http://data.doremus.org/ontology#>
     PREFIX ecrm: <http://erlangen-crm.org/current/>
@@ -54,10 +100,14 @@ if __name__ == '__main__':
     }
     """
 
-    qres = g1.query(q1)
+    q3 = """
+    
+    """
 
-    for row in qres:
-        print(f"{row.title}")
+    #qres = g1.query(q1)
+
+    #for row in qres:
+    #    print(f"{row.title}")
 
     qres = g1.query(q2)
 
@@ -65,11 +115,18 @@ if __name__ == '__main__':
         print(f"{row.property}")
 
     # Intersection entre deux graphes
-    gInter = g1 & g2
+    #gInter = g1 & g2
     #print("Intersection : ")
     #getRelationTypes(gInter)
 
-    intersection = existSame(s1, s2)
+    #intersection = existSame(s1, s2)
+
+    #getAllTypes(g1)
+    #getAllTypes(g2)
+
+    getAllGenres(g1)
+    getAllGenres(g2)
+
     #for s, p, o in g1.triples((None, None, "F22_Self-Contained_Expression")):
         #print(o.hasTitle)
 
